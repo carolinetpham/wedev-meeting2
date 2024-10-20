fd565a00923890ceb141b182b46740785fe6f989
Caroline Pham <carolinetpham@outlook.com>
2024-10-20 13:59:20 -0400

    Initial Commit

---
+++ WeDev/WeDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeDevApp.swift
+//  WeDev
+//
+//  Created by Caroline Pham on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WeDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
